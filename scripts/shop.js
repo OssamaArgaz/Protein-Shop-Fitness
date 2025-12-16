67c97668fe8408d47dcda9bf4307b8641dada9f7
ossama argaz <ossama2323>
2025-12-16 23:59:00 +0100

    l'ajoute de filter sous forme d'un offcanvas pour responsive devices

--- scripts/shop.js
+++ scripts/shop.js
@@ -16,17 +16,37 @@ const range = document.querySelector('.slider_track');
 const sliderMinValue = parseInt(minValue.min);
 const sliderMaxValue = parseInt(maxValue.max);
 
+const responsiveMinValue = document.querySelector('.responsiveMin_value');
+const responsiveMaxValue = document.querySelector('.responsiveMax_value');
+const responsivePriceInpuMin = document.querySelector('.responsiveMin_input');
+const responsivePriceInpuMax = document.querySelector('.responsiveMax_input');
+const responsiveMinTooltip = document.querySelector('.responsiveMin_tooltipe');
+const responsiveMaxTooltip = document.querySelector('.responsiveMax_tooltipe');
+const responsiveMinGap = 0;
+const responsiveRange = document.querySelector('.responsiveSlider_track');
+const responsiveSliderMinValue = parseInt(responsiveMinValue.min);
+const responsiveSliderMaxValue = parseInt(responsiveMaxValue.max);
+
 
 
 function slideMin(){
     let gap = parseInt(maxValue.value) - parseInt(minValue.value);
-
     if(gap <= minGap){
         minValue.value = parseInt(maxValue.value) - minGap;
     }
-
     minTooltip.innerHTML = "$" + minValue.value;
     priceInpuMin.value = minValue.value;
+
+
+
+    let responsivegap = parseInt(responsiveMaxValue.value) - parseInt(responsiveMinValue.value);
+    if(responsivegap <= responsiveMinGap){
+        responsiveMinValue.value = parseInt(responsiveMaxValue.value) - responsiveMinGap;
+    }
+    responsiveMinTooltip.innerHTML = "$" + responsiveMinValue.value;
+    responsivePriceInpuMin.value = responsiveMinValue.value;
+
+
 
     setArea();
 }
@@ -34,22 +54,34 @@ function slideMin(){
 
 function slideMax(){
     let gap = parseInt(maxValue.value) - parseInt(minValue.value);
-
     if(gap <= minGap){
         maxValue.value = parseInt(minValue.value) + minGap;
     }
-
     maxTooltip.innerHTML = "$" + maxValue.value;
     priceInpuMax.value = maxValue.value;
+
+
+    let responsiveGap = parseInt(responsiveMaxValue.value) - parseInt(responsiveMinValue.value);
+    if(responsiveGap <= responsiveMinGap){
+        responsiveMaxValue.value = parseInt(responsiveMinValue.value) + responsiveMinGap;
+    }
+    responsiveMaxTooltip.innerHTML = "$" + responsiveMaxValue.value;
+    responsivePriceInpuMax.value = responsiveMaxValue.value;
 
     setArea();
 }
 
 function setArea(){
+    responsiveRange.style.left = (responsiveMinValue.value / responsiveSliderMaxValue) * 100 + "%";
+    responsiveMinTooltip.style.left = (responsiveMinValue.value / responsiveSliderMaxValue) * 100 + "%";
+    responsiveRange.style.right = 100 - (responsiveMaxValue.value / responsiveSliderMaxValue) * 100 + "%";
+    responsiveMaxTooltip.style.right = 100 - (responsiveMaxValue.value / responsiveSliderMaxValue) * 100 + "%";
+
     range.style.left = (minValue.value / sliderMaxValue) * 100 + "%";
     minTooltip.style.left = (minValue.value / sliderMaxValue) * 100 + "%";
     range.style.right = 100 - (maxValue.value / sliderMaxValue) * 100 + "%";
     maxTooltip.style.right = 100 - (maxValue.value / sliderMaxValue) * 100 + "%";
+    
 }
 
 
@@ -60,6 +92,13 @@ function setMinInput(){
     }
     minValue.value = priceInpuMin.value;
 
+
+    let responsiveMinPrice = parseInt(responsivePriceInpuMin.value);
+    if(responsiveMinPrice < responsiveSliderMinValue){
+        responsivePriceInpuMin.value = responsiveSliderMinValue;
+    }
+    responsiveMinValue.value = responsivePriceInpuMin.value;
+
     slideMin();
 }
 
@@ -69,6 +108,13 @@ function setMaxInput(){
         priceInpuMax.value = sliderMaxValue;
     }
     maxValue.value = priceInpuMax.value;
+
+        
+    let responsiveMaxPrice = parseInt(responsivePriceInpuMax.value);
+    if(responsiveMaxPrice > responsiveSliderMaxValue){
+        responsivePriceInpuMax.value = responsiveSliderMaxValue;
+    }
+    responsiveMaxValue.value = responsivePriceInpuMax.value;
 
     slideMax();
 }
