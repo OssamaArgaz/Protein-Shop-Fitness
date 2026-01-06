e970abfe806753ada16083cc9d02f61f842c3793
ossama argaz <ossama2323>
2026-01-06 09:23:20 +0100

    ajoute d'une liste des produites recement consultés ou visite

--- scripts/product.js
+++ scripts/product.js
@@ -1,4 +1,53 @@
+// Related Products Cards 
 var latest_deals_cards = $('.related-product-cards');
+
+latest_deals_cards.owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout: 3500,
+    margin: 10,
+    nav:true,
+    navText:["<i class='icofont-arrow-left'></i>",
+            "<i class='icofont-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:2
+        },
+        992:{
+            items:3
+        },
+        1150:{
+            items:4
+        }
+    }
+});
+latest_deals_cards.on('mouseover', function(){
+    latest_deals_cards.trigger('stop.owl.autoplay');
+});
+latest_deals_cards.on('mouseout', function(){
+    latest_deals_cards.trigger('play.owl.autoplay');
+});
+
+
+
+
+// related product card info
+document.addEventListener('DOMContentLoaded', function() {
+  var popoverElements = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+
+  popoverElements.map(function(popoverEl){
+    return new bootstrap.Popover(popoverEl);
+  });
+});
+
+
+
+// Recent Products Cards 
+
+var latest_deals_cards = $('.recent-product-cards');
 
 latest_deals_cards.owlCarousel({
     loop:true,
